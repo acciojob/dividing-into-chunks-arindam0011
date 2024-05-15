73aaa91a4929816ecef3ff3659e60a6e7cbe01b0
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-16 00:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
-	let sum=0;
-	let st=0;
-	let ans=[];
-	for (let i=0; i<arr.length, i++){
-		  sum+=arr[i];
-		 if(sum>m){
-			 let sub= arr.slice(st,i);
-			 ans.push(sub);
-			 st=i;
-			 sum=0;
-		 }
-	}
-	console.log(ans);
+    let sum = 0;
+    let st = 0;
+    let ans = [];
+    for(let i = 0; i <arr.length; i++) {
+        sum += arr[i];
+        if (sum > n) {
+            let sub = arr.slice(st, i);
+            ans.push(sub);
+            st = i;
+            sum = arr[i];
+        }
+    }
+    let sub2= arr.slice(st, arr.length);
+    ans.push(sub2);
+    return ans;
 };
 
 const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+const result = divide(arr, parseInt(n));
+alert(JSON.stringify(result));
