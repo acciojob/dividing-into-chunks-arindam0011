f09cb49b7d55566d6fc9cbcaa1b6373ca893b1d7
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-05-15 23:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let sum=0;
+	let st=0;
+	let ans=[];
+	for (let i=0; i<arr.length, i++){
+		  sum+=arr[i];
+		 if(sum>m){
+			 let sub= arr.slice(st,i);
+			 ans.push(sub);
+			 st=i;
+			 sum=0;
+		 }
+	}
+	console.log(ans);
 };
 
 const n = prompt("Enter n: ");
